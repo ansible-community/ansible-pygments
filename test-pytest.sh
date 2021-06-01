dd38c9d7e37012fb99bfb123e313f5d22bc2fe8a
Felix Fontein <felix@fontein.de>
2021-06-01 13:29:10 +0200

    Cleanup.

--- test-pytest.sh
+++ test-pytest.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 set -e
-poetry run python -W 'ignore:"@coroutine" decorator is deprecated::asynctest.case' \
-	-m pytest --cov-branch --cov=sphinx_ansible_highlighter --cov-report term-missing -vv tests "$@"
+poetry run python -m pytest --cov-branch --cov=sphinx_ansible_highlighter --cov-report term-missing -vv tests "$@"
