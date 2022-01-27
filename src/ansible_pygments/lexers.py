a821e0f304785d87365fb3cd7d1e4a8837ca66ec
Felix Fontein <felix@fontein.de>
2022-01-27 13:06:25 +0100

    Fix CI (#25)
    * Shut up pylint on f-strings in lexers.py.
    
    * Adjust to output of newer pygments version.

--- src/ansible_pygments/lexers.py
+++ src/ansible_pygments/lexers.py
@@ -34,6 +34,8 @@
 
 """Pygments lexers for ansible console output."""
 
+# pylint: disable=consider-using-f-string
+
 from pygments import token
 from pygments.lexer import DelegatingLexer, RegexLexer, bygroups, include
 from pygments.lexers import DiffLexer  # pylint: disable=no-name-in-module
